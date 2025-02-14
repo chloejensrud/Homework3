8683b416569632037e76f2469af5549f262d074b
chloejensrud <ij1247ov@go.minnstate.edu>
2025-02-13 21:17:46 -0600

    Add files via upload

---
+++ Manticore.cs
@@ -0,0 +1,39 @@
+﻿// Written By Chloe Jensrud
+// 2/11/25
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manticore
+{
+    public class Manticore
+    {
+        private int distance = -1;
+        private int health = -1;
+
+        public int Distance 
+        {   get { return distance; } set { distance = value; } 
+        }
+        public int Health
+        {
+            get { return health; } set { health = value; }
+        }
+
+        public Manticore(int distance)
+        { this.distance = distance; }
+
+        public void TakeDamake(int damage)
+        {
+            this.health -= damage;
+            if (this.health < 0)
+            {
+                this.health = 0;
+            }
+        }
+
+    }
+}
---
+++ ManticoreProgram.cs
@@ -0,0 +1,68 @@
+﻿// Written by Chloe Jensrud
+// 2/11/25
+
+namespace Manticore
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Player 1, please set the Manticore distance from the city (0-100): ");
+            int distance = GetDistance();
+            Console.Clear();
+
+            Manticore aManticore = new Manticore(distance);
+            int cityHealth = 15;
+            int round = 1;
+
+            while ( cityHealth > 0 && aManticore.Health > 0 )
+            {
+                Console.WriteLine($"Round {round}");
+                Console.WriteLine($"City Health: {cityHealth} | Manticore Health: {aManticore.Health}");
+
+                int cannonDamage = CalculateCannonDamage(round);
+                Console.WriteLine($"This round the cannon will deal {cannonDamage} damage.");
+
+                Console.WriteLine("Player 2, enter the cannons firing distance: ");
+                int playerGuess = GetDistance();
+
+                if (playerGuess == aManticore.Distance)
+                {
+                    Console.WriteLine("That was a DIRECT HIT!!");
+                    aManticore.TakeDamake(cannonDamage);
+                }
+                if (playerGuess < aManticore.Distance)
+                {
+                    Console.WriteLine("The shot fell SHORT.");
+                }
+                if (playerGuess > aManticore.Distance)
+                {
+                    Console.WriteLine("The shot OVERSHOT the target.");
+                }
+                if (aManticore.Health > 0)
+                {
+                    cityHealth--;
+                }
+                round++;
+            }
+            Console.WriteLine(cityHealth > 0
+                ? "The Manticore has been destroyed and the city of Consolas has been saved!"
+                : "The city has been destroyed! The Manticore wins!");
+        }
+        static int GetDistance()
+        {
+            int distance;
+            while (!int.TryParse(Console.ReadLine(), out distance) || distance < 0 || distance > 100 )
+            {
+                Console.WriteLine("Invalid input, select a number between 0 - 100");
+            }
+            return distance;
+        }
+        private static int CalculateCannonDamage(int round)
+        {
+            if (round % 3 == 0 && round % 5 == 0) return 10;
+            if(round % 3 == 0 ||  round % 5 == 0) return 3;
+            return 1;
+        }
+    }
+}
