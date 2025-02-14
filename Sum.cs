b47cee37028d97da1ee5feb3a0123e8d730abb06
chloejensrud <ij1247ov@go.minnstate.edu>
2025-02-13 21:19:14 -0600

    Add files via upload

---
+++ Sum.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumOfAbsoluteValue
+{
+    public class Sum
+    {
+        public int GetAbsSum(int[] arr)
+        {
+            int sum = 0;
+
+            foreach (int num in arr)
+            { sum += Math.Abs(num); }
+            return sum;
+        }
+    }
+}
---
+++ SumProgram.cs
@@ -0,0 +1,14 @@
+﻿namespace SumOfAbsoluteValue
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sum aSum = new Sum();
+            
+            int[] test = new int[] { 2, -1, 4, 8, -10 };
+
+            Console.WriteLine(aSum.GetAbsSum(test));
+        }
+    }
+}
