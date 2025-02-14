6c6cb0934e9a4029470647be778ffe7b163d1a1c
chloejensrud <ij1247ov@go.minnstate.edu>
2025-02-13 21:16:28 -0600

    Add files via upload

---
+++ Numbers.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindMinMax1
+{
+    public class Numbers
+    {
+        public int[] FindMinMax(int[] arr)
+        {
+            int min = arr[0];
+            int max = arr[0];
+
+            foreach (int num in arr)
+            {
+                if (num < min) min = num;
+                if (num > max) max = num;
+            }
+            return new int[] { min, max };
+
+        }
+    }
+}
---
+++ NumbersProgram.cs
@@ -0,0 +1,15 @@
+﻿namespace FindMinMax1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Numbers aNumbers = new Numbers();
+
+            int[] aNumber1 = new int[] {1, 2, 3, 4, 5};
+            int[] result = aNumbers.FindMinMax(aNumber1);
+
+            Console.WriteLine($"{result[0]}, {result[1]}");
+        }
+    }
+}
